d625e9bd0e5da96059821f2d56f82261a1ca91ec
Ege Furkan Bilgin <furkanegebilgin@gmail.com>
2019-12-13 17:53:11 +0300

    Implemented RequestHandler for movies and directors.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,26 +1,56 @@
 import static spark.Spark.*;
+import com.google.gson.Gson;
 
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Main");
-		
+		RequestHandler requestHandler = new RequestHandler();
+		requestHandler.handlePaths();
 	}
 }
 
 class RequestHandler {
 	int port = 8080;
-	Database database;
+	private Database database;
+	private Gson gson = new Gson();
 
 	public RequestHandler() {
 		database = Database.getInstance();
 		port(port);
-		handleGetRequests();
 	}
 
-	void handleGetRequests() {
-		get("/movies", (request, response) -> "getAllMovies");
-		get("/movies/:id", (request, response) -> {
-			return database.getMovie(request.params(":id"));
+	void handlePaths() {
+		path("/api", () -> {
+			before("/*", (q, a) -> System.out.println("Received api call"));
+			path("/movie", () -> {
+				get("/get/id/:id", (request, response) -> {
+					return database.getMovie(request.params(":id")).toJSON();
+				});
+				get("/get/all", (request, response) -> {
+					System.out.println("getAllMovies");
+					return gson.toJson(database.getAllMovies());
+				});
+				post("/add", (request, response) -> {
+					database.addMovie(gson.fromJson(request.body(), Movie.class));
+					return null;
+				});
+				post("/set", (request, response) -> {
+					database.setMovie(gson.fromJson(request.body(), Movie.class));
+					return null;
+				});
+			});
+			path("/director", () -> {
+				get("/get/id/:id", (request, response) -> database.getDirector(request.params(":id")));
+				get("/get/all", (request, response) -> gson.toJson(database.getAllDirectors()));
+				post("/add", (request, response) -> {
+					database.addDirector(gson.fromJson(request.body(), Director.class));
+					return null;
+				});
+				post("/set", (request, response) -> {
+					database.setDirector(gson.fromJson(request.body(), Director.class));
+					return null;
+				});
+			});
 		});
 	}
 }
