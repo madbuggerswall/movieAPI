c09a42d9b2a23fd934b1f9f91aebc53e339d8862
Ege Furkan Bilgin <furkanegebilgin@gmail.com>
2019-12-14 19:11:02 +0300

    Oops! Turns out CORS was working only in localhost.

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -12,6 +12,25 @@ class RequestHandler {
 	}
 
 	void setResponseHeaders() {
+		options("/*",
+        (request, response) -> {
+
+            String accessControlRequestHeaders = request
+                    .headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers",
+                        accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request
+                    .headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods",
+                        accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
 		before((request, response) -> {
 			response.header("Access-Control-Allow-Origin", "*");
 			response.header("Access-Control-Request-Method", "GET");
