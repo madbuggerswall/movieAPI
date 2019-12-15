1604cebba17ce29f574b7b7276009a357a3cad92
Ege Furkan Bilgin <furkanegebilgin@gmail.com>
2019-12-16 01:08:17 +0300

    Options request does not send back "Access-Control-Allow-Origin" header now. It caused "Multiple CORS header ‘Access-Control-Allow-Origin’ not allowed" error because DELETE and PUT methods were requesting preflight OPTIONS.

--- movieAPI.js
+++ movieAPI.js
@@ -4,108 +4,112 @@ let url = "http://ec2-52-56-129-37.eu-west-2.compute.amazonaws.com:8888/";
 
 // Movie
 function addMovie(data) {
-	return post("/movies", data);
+	return post("movies", data);
 }
 
 function deleteMovie(id) {
-	return deleteDoc("/movies/" + id);
+	return del("movies/" + id);
 }
 
 function getMovie(id) {
-	return get("/movies/" + id);
+	return get("movies/" + id);
 }
 
 function getAllMovies() {
-	return get("/movies");
+	return get("movies");
 }
 
 function updateMovie(id, data) {
-	return put("/movies/" + id, data);
+	return put("movies/" + id, data);
 }
 
 // Director
 function addDirector(data) {
-	return post("/directors", data)
+	return post("directors", data);
 }
 
 function deleteDirector(id) {
-	return deleteDoc("/directors/" + id);
+	return del("directors/" + id);
 }
 
 function getDirector(id) {
-	return get("/directors/" + id);
+	return get("directors/" + id);
 }
 
 function getAllDirectors() {
-	return get("/directors");
+	return get("directors");
 }
 
 function updateDirector(id, data) {
-	return put("/directors/" + id, data);
+	return put("directors/" + id, data);
 }
 
 // User
 function addUser(data) {
-	return post("/users", data);
+	return post("users", data);
 }
 
 function addMovieToUserList(userID, listIndex, movieID) {
-	return get("/users/" + userID + "/" + listIndex + "/" + movieID);
+	return get("users/" + userID + "/" + listIndex + "/" + movieID);
 }
 
 function deleteUser(id) {
-	return deleteDoc("/users/" + id);
+	return del("users/" + id);
 }
 
 function getUser(id) {
-	return get("/users/" + id);
+	return get("users/" + id);
 }
 
 function getAllUsers() {
-	return get("/users");
+	return get("users");
 }
 
 function hasLoggedIn(id, token) {
-	return post("/users/hasLoggedIn/" + id, token);
+	return post("users/hasLoggedIn/" + id, token);
 }
 
 function loginUser(userDTO) {
-	return post("/users/login", userDTO);
+	return post("users/login", userDTO);
 }
 
 function updateUser(id, data) {
-	return put("/users/" + id, data);
+	return put("users/" + id, data);
 }
 
+// Generic request method functions.
 function get(query) {
 	return request("GET", query);
 }
 
-// Generic request method functions.
 function post(query, data) {
-	return request("POST", query, data);
+	return requestSend("POST", query, data);
 }
 
 function put(query, data) {
-	return request("PUT", query, data);
+	return requestSend("PUT", query, data);
 }
 
-function deleteDoc(query) {
-	return request("DELETE", query)
+function del(query) {
+	return request("DELETE", query);
 }
 
 // Generic request functions.
 function request(type, query) {
 	let xmlHttp = new XMLHttpRequest();
 	let requestURL = url + query;
+  console.log("2 "+ requestURL);
 	xmlHttp.open(type, requestURL, false); // False for synchronous request.
 	xmlHttp.send();
 	return xmlHttp.responseText;
 }
-function request(type, query, data) {
+
+function requestSend(type, query, data) {
 	let xmlHttp = new XMLHttpRequest();
 	let requestURL = url + query;
+  console.log("3 "+ requestURL);
 	xmlHttp.open(type, requestURL, false);
 	xmlHttp.send(data);
 	return xmlHttp.responseText;
 }
+
